6cc21c624284ba717cfbeedc93a6100337bfca95
CeferinSoftware <newshellweb@gmail.com>
2025-07-14 19:49:06 +0200

    Update lib/config/ai_config.dart with real app data - 2025-07-14 17:49:06

---
+++ lib/config/ai_config.dart
@@ -0,0 +1,7 @@
+class AIConfig {
+  static const String provider = 'gemini';
+  static const String model = 'gemini-1.5-flash';
+  static const String apiKey = 'AIzaSyAy-IS0ZPVF4Z-IAD7zrwWMkybIfM4LVFQ';
+  static const String chatbotName = 'PokeWokeAI';
+  static const String systemPrompt = 'Eres un asistente online';
+}
